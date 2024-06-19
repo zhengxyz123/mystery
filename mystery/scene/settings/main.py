3d522fd8807a5f6d89a8409e8436b6ac8cc1647c
zhengxyz123 <3422794023@qq.com>
2024-06-19 21:07:02 +0800

    Add a "control" button in settings.

--- mystery/scene/settings/main.py
+++ mystery/scene/settings/main.py
@@ -31,45 +31,54 @@ class SettingsScene(Scene):
 
         self.buttons_frame = AdvancedFrame(
             self.window.resource.translate("settings.title"),
-            self.window.width // 2 - 140,
-            self.window.height // 2 - 149,
-            280,
-            319,
+            self.window.width // 2 - 150,
+            self.window.height // 2 - 182,
+            300,
+            385,
             batch=self.batch,
             group=self.mid_group,
         )
         self.game_button = TextButton(
             self.window.resource.translate("settings.game"),
-            self.window.width // 2 - 120,
-            self.window.height // 2 + 48,
-            240,
+            self.window.width // 2 - 130,
+            self.window.height // 2 + 77,
+            260,
+            55,
+            batch=self.batch,
+            group=self.fore_group,
+        )
+        self.control_button = TextButton(
+            self.window.resource.translate("settings.control"),
+            self.window.width // 2 - 130,
+            self.window.height // 2 + 18,
+            260,
             55,
             batch=self.batch,
             group=self.fore_group,
         )
         self.graphic_button = TextButton(
             self.window.resource.translate("settings.graphic"),
-            self.window.width // 2 - 120,
-            self.window.height // 2 - 12,
-            240,
+            self.window.width // 2 - 130,
+            self.window.height // 2 - 42,
+            260,
             55,
             batch=self.batch,
             group=self.fore_group,
         )
         self.sound_button = TextButton(
             self.window.resource.translate("settings.sound"),
-            self.window.width // 2 - 120,
-            self.window.height // 2 - 72,
-            240,
+            self.window.width // 2 - 130,
+            self.window.height // 2 - 102,
+            260,
             55,
             batch=self.batch,
             group=self.fore_group,
         )
         self.language_button = TextButton(
             self.window.resource.translate("settings.language"),
-            self.window.width // 2 - 120,
-            self.window.height // 2 - 132,
-            240,
+            self.window.width // 2 - 130,
+            self.window.height // 2 - 162,
+            260,
             55,
             batch=self.batch,
             group=self.fore_group,
@@ -81,6 +90,7 @@ class SettingsScene(Scene):
         self.frame.add_widget(
             self.buttons_frame,
             self.game_button,
+            self.control_button,
             self.graphic_button,
             self.sound_button,
             self.language_button,
@@ -108,15 +118,17 @@ class SettingsScene(Scene):
             self.background.scale = width / self.background.image.width
         else:
             self.background.scale = height / self.background.image.height
-        self.buttons_frame.position = (width // 2 - 140, height // 2 - 149)
-        self.game_button.position = (width // 2 - 120, height // 2 + 48)
-        self.graphic_button.position = (width // 2 - 120, height // 2 - 12)
-        self.sound_button.position = (width // 2 - 120, height // 2 - 72)
-        self.language_button.position = (width // 2 - 120, height // 2 - 132)
+        self.buttons_frame.position = (width // 2 - 150, height // 2 - 182)
+        self.game_button.position = (width // 2 - 130, height // 2 + 77)
+        self.control_button.position = (width // 2 - 130, height // 2 + 18)
+        self.graphic_button.position = (width // 2 - 130, height // 2 - 42)
+        self.sound_button.position = (width // 2 - 130, height // 2 - 102)
+        self.language_button.position = (width // 2 - 130, height // 2 - 162)
 
     def on_language_change(self):
         self.buttons_frame.title = self.window.resource.translate("settings.title")
         self.game_button.text = self.window.resource.translate("settings.game")
+        self.control_button.text = self.window.resource.translate("settings.control")
         self.graphic_button.text = self.window.resource.translate("settings.graphic")
         self.sound_button.text = self.window.resource.translate("settings.sound")
         self.language_button.text = self.window.resource.translate("settings.language")
